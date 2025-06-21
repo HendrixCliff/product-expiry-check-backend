78ef686a7caa7e34a2c37bac9994a6f4ac066116
Godwin Chukwuebuka Igwegbe <godwincliff10@email.com>
2025-06-21 15:17:45 +0100

    Remove hardcoded port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('./config/db');
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT 
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
